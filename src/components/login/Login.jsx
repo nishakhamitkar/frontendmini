1292635567cf0409442fdb68882b0d01a3243afc
[pooja kosandar] <[pooja.kosandar21@gmail.com]>
2026-02-27 16:47:49 +0530

    k

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 import Axios from "../../axios/Axios";
 
 const Login = ({onlogin}) => {
